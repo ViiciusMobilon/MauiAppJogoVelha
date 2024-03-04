dd4273bc0343f3c4865ebb5fe7ec41b088768ad2
Usuario <Usuario@CPD03-M07>
2024-03-04 15:00:30 -0300

    jogo da velha 2.0

--- MauiAppJogoVelha/MainPage.xaml.cs
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -2,12 +2,37 @@
 {
     public partial class MainPage : ContentPage
     {
+        string vez = "O";
+
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+        Button disparador = (Button)sender;
+
+            disparador.IsEnabled = false;
+
+            if (vez == "O") 
+            {
+            disparador.Text = "0";
+                vez = "X";
+            } else
+            {
+                disparador.Text="X";
+                vez = "O";
+
+
+            }
+
+
+
+        }
+
+       
     }
 
 }
